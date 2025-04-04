cf3af566f81f23766dbaf76fee8d8af09b626f9e
THungB <tienhung2000dt@gmail.com>
2025-04-04 21:46:54 +0700

    new

--- error_page.php
+++ error_page.php
@@ -1,27 +1,28 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Application Submitted</title>
     <link rel="stylesheet" href="styles\sumbit.css">
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css">
 </head>
-
 <body>
 
 <div class="wrapper" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="300" data-aos-offset="0" data-aos-duration="500">
-<div class="alert-success">
-    <div class="alert-icon" class="alert-success" >
-      <ion-icon class="icon" name="checkmark"></ion-icon>
+<div class="alert-error">
+    <div class="alert-icon">
+       <ion-icon class="icon" name="close"></ion-icon>
     </div>
     <div class="content">
-       <h1>Application Submitted Successfully</h1>
-       <p>Thank you for your expression of interest. Your EOI number is: <strong><?php echo $eoiNumber; ?></strong></p>
-       <p>We will contact you soon regarding your application.</p>
-    </div>
-    <a href="index.php">
-      <button>Roll back</button>
+       <h1>Application Submission Error</h1>
+    <p>Please correct the following errors:</p>
+    <ul>
+        <?php foreach ($errors as $field => $message): ?>
+            <li><strong><?php echo ucfirst(str_replace('_', ' ', $field)); ?>:</strong> <?php echo $message; ?></li>
+        <?php endforeach; ?>
+    </ul>
+    <a href="apply.php">
+      <button>Try again</button>
     </a>
 </div>
 </div>  
